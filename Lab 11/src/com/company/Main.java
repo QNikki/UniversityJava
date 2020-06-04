5bb15310867bede41a6a287bb50679cde0595dce
Dmitry Konovslov <dima.konovalov3424@gmail.com>
2020-06-04 17:38:03 +0300

    Add formuls

--- Lab 11/src/com/company/Main.java
+++ Lab 11/src/com/company/Main.java
@@ -5,18 +5,21 @@ import java.math.*;
 public class Main {
 
     public static void main(String[] args) {
-
-        BigInteger a = BigInteger.valueOf(2147483);
-        BigInteger b = BigInteger.valueOf(1289346);
-        a = a.multiply(b);
-        System.out.println(a);
-        long x = a.longValue();
-        System.out.println(x);
-        BigDecimal c = BigDecimal.valueOf(18925.06);
-        BigDecimal d = BigDecimal.valueOf(23451.06);
-        c = c.subtract(d.multiply(BigDecimal.valueOf(2.1)));
+        System.out.println("c = sqrt(a^2, b^2) bigInt");
+        BigInteger a = BigInteger.valueOf(214743);
+        BigInteger b = BigInteger.valueOf(128946);
+        a = a.multiply(a);
+        b = b.multiply(b);
+        BigInteger c = a.add(b).sqrt();
         System.out.println(c);
-        double y = c.doubleValue();
-        System.out.println(y);
+        System.out.println("a^2 + b^2 + 2*a*b = c");
+        BigDecimal a1 = BigDecimal.valueOf(18925.06);
+        BigDecimal b1 = BigDecimal.valueOf(23451.06);
+        BigDecimal ab2 = a1.multiply(b1);
+        ab2 = ab2.add(ab2);
+        a1 = a1.multiply(a1);
+        b1 = b1.multiply(b1);
+        BigDecimal c1 = a1.add(b1).add(ab2);
+        System.out.println(c1);
     }
 }
